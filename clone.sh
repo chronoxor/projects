50f445a9eb44fa77ee0ba355cc178507886fbef2
Ivan Shynkarenka <chronoxor@gmail.com>
2016-06-22 19:57:35 +0300

    Add CppLogging project

--- clone.sh
+++ clone.sh
@@ -3,3 +3,4 @@ git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
 git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
 git clone "git@bitbucket.org:chronoxor/CppTemplate.git" CppTemplate
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
+git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
