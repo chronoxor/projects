12a07a8f0cd3df65cbafee1edea56fc837a845fb
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-09 22:58:56 +0300

    Update copy scripts

--- clone.bat
+++ clone.bat
@@ -1,6 +1,17 @@
 git clone "git@github.com:chronoxor/CppBuildScripts.git" CppBuildScripts
+git submodule update --init --recursive
+
 git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
+git submodule update --init --recursive
+
 git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
+git submodule update --init --recursive
+
 git clone "git@bitbucket.org:chronoxor/CppTemplate.git" CppTemplate
+git submodule update --init --recursive
+
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
+git submodule update --init --recursive
+
 git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
+git submodule update --init --recursive
