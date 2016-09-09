b6928483e29c91f7c7b06eab44507d36099fc383
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-09 23:06:59 +0300

    Update clone scripts

--- clone.bat
+++ clone.bat
@@ -1,17 +1,14 @@
 git clone "git@github.com:chronoxor/CppBuildScripts.git" CppBuildScripts
-git submodule update --init --recursive
-
 git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
-git submodule update --init --recursive
-
 git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppTemplate.git" CppTemplate
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
-git submodule update --init --recursive
+
+FOR /D %%X IN (
+  *
+) DO (
+  cd %%X
+  git submodule update --init --recursive
+  cd ..
+)
--- clone.sh
+++ clone.sh
@@ -1,17 +1,13 @@
 git clone "git@github.com:chronoxor/CppBuildScripts.git" CppBuildScripts
-git submodule update --init --recursive
-
 git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
-git submodule update --init --recursive
-
 git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppTemplate.git" CppTemplate
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
-git submodule update --init --recursive
-
 git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
-git submodule update --init --recursive
+
+for project in */
+do
+  cd ./$project
+  git submodule update --init --recursive
+  cd ..
+done
