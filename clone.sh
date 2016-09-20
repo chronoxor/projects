61b98236d47e0132edbe4c95d8353d2f2945c17f
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-20 04:11:41 +0300

    Add CppTemplate project

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 git clone "git@github.com:chronoxor/CppBuildScripts.git" CppBuildScripts
 git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
 git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
-git clone "git@bitbucket.org:chronoxor/CppTemplate.git" CppTemplate
+git clone "git@github.com:chronoxor/CppTemplate.git" CppTemplate
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
 git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
 
