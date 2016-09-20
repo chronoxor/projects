9eb647c05250956808d521d1bcc92dc43e82d371
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-20 04:54:53 +0300

    Update build scripts

--- clone.bat
+++ clone.bat
@@ -1,7 +1,7 @@
-git clone "git@github.com:chronoxor/CppBuildScripts.git" CppBuildScripts
-git clone "git@github.com:chronoxor/CppCMakeScripts.git" CppCMakeScripts
-git clone "git@github.com:chronoxor/CppBenchmark.git" CppBenchmark
-git clone "git@github.com:chronoxor/CppTemplate.git" CppTemplate
+git clone "https://github.com/chronoxor/CppBuildScripts.git" CppBuildScripts
+git clone "https://github.com/chronoxor/CppCMakeScripts.git" CppCMakeScripts
+git clone "https://github.com/chronoxor/CppBenchmark.git" CppBenchmark
+git clone "https://github.com/chronoxor/CppTemplate.git" CppTemplate
 git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
 git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
 
