8598abc43a8f1c5d1d45d448664078c8c5934479
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-22 18:38:04 +0300

    Update

--- clone.bat
+++ clone.bat
@@ -2,8 +2,8 @@ git clone "https://github.com/chronoxor/CppBuildScripts.git" CppBuildScripts
 git clone "https://github.com/chronoxor/CppCMakeScripts.git" CppCMakeScripts
 git clone "https://github.com/chronoxor/CppBenchmark.git" CppBenchmark
 git clone "https://github.com/chronoxor/CppTemplate.git" CppTemplate
-git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
-git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
+rem git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
+rem git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
 
 FOR /D %%X IN (
   *
--- clone.sh
+++ clone.sh
@@ -2,8 +2,8 @@ git clone "https://github.com/chronoxor/CppBuildScripts.git" CppBuildScripts
 git clone "https://github.com/chronoxor/CppCMakeScripts.git" CppCMakeScripts
 git clone "https://github.com/chronoxor/CppBenchmark.git" CppBenchmark
 git clone "https://github.com/chronoxor/CppTemplate.git" CppTemplate
-git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
-git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
+#git clone "git@bitbucket.org:chronoxor/CppCommon.git" CppCommon
+#git clone "git@bitbucket.org:chronoxor/CppLogging.git" CppLogging
 
 for project in */
 do
