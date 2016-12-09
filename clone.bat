b6c34081c7e6aba07f376bacf36546dcff8b7edd
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-09 15:16:04 +0300

    Add CppServer project

--- clone.bat
+++ clone.bat
@@ -4,6 +4,7 @@ git clone "https://github.com/chronoxor/CppBenchmark.git" CppBenchmark
 git clone "https://github.com/chronoxor/CppTemplate.git" CppTemplate
 git clone "https://github.com/chronoxor/CppCommon.git" CppCommon
 git clone "https://github.com/chronoxor/CppLogging.git" CppLogging
+git clone "https://github.com/chronoxor/CppServer.git" CppServer
 
 for /D %%X in (
   *
