f5b9002e4a1a21333c310af45ea82e43e30a124c
Ivan Shynkarenka <chronoxor@gmail.com>
2018-08-08 18:29:03 +0300

    update

--- clone.bat
+++ clone.bat
@@ -7,6 +7,7 @@ git clone "https://github.com/chronoxor/CppLogging.git" CppLogging
 git clone "https://github.com/chronoxor/CppSerialization.git" CppSerialization
 git clone "https://github.com/chronoxor/CppServer.git" CppServer
 git clone "https://github.com/chronoxor/CppTrader.git" CppTrader
+git clone "https://github.com/chronoxor/CSharpServer.git" CSharpServer
 
 for /D %%X in (
   *
