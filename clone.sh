6430a35a106767ae7822c0820c1531840ffd64e2
Ivan Shynkarenka <chronoxor@gmail.com>
2018-08-24 02:04:59 +0300

    update

--- clone.sh
+++ clone.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-git clone "https://github.com/chronoxor/CppBuildScripts.git"
-git clone "https://github.com/chronoxor/CppCMakeScripts.git"
-git clone "https://github.com/chronoxor/CppBenchmark.git"
-git clone "https://github.com/chronoxor/CppTemplate.git"
-git clone "https://github.com/chronoxor/CppCommon.git"
-git clone "https://github.com/chronoxor/CppLogging.git"
-git clone "https://github.com/chronoxor/CppSerialization.git"
-git clone "https://github.com/chronoxor/CppServer.git"
-git clone "https://github.com/chronoxor/CppTrader.git"
-git clone "https://github.com/chronoxor/CSharpServer.git"
-git clone "https://github.com/chronoxor/FastBinaryEncoding.git"
+git clone https://github.com/chronoxor/CppBuildScripts.git
+git clone https://github.com/chronoxor/CppCMakeScripts.git
+git clone https://github.com/chronoxor/CppBenchmark.git
+git clone https://github.com/chronoxor/CppTemplate.git
+git clone https://github.com/chronoxor/CppCommon.git
+git clone https://github.com/chronoxor/CppLogging.git
+git clone https://github.com/chronoxor/CppSerialization.git
+git clone https://github.com/chronoxor/CppServer.git
+git clone https://github.com/chronoxor/CppTrader.git
+git clone https://github.com/chronoxor/CSharpServer.git
+git clone https://github.com/chronoxor/FastBinaryEncoding.git
 
 for project in */
 do
